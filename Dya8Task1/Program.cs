4241c52e4ce93ed761f05654b646726c33dcfebd
Alexandr <digilevich.alexandr@gmail.com>
2015-08-03 14:31:28 +0300

    2

---
+++ Dya8Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dya8Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] iarray= new int[10,5];
+            Random r = new Random(10);
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 5; j++)
+                    iarray[i,j]=r.Next(10);
+            Matrix<int> m = new Matrix<int>(10,5,iarray);
+            Console.WriteLine(m);
+            m.Register(m);
+            m.ChangeSingleCell(4, 2,777);
+            Console.WriteLine();            
+
+            double[,] darray = new double[3, 3];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    darray[i, j] =r.Next(10);
+            SquereMatrix<double> sqm = new SquereMatrix<double>(3,darray);
+            Console.WriteLine(sqm);
+            sqm.ChangeSingleCell(2, 2, 77.7);
+            Console.WriteLine();
+            
+            //конструктор отзеркалит нижнюю половину
+            string[,] strarray = new string[3, 3];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    strarray[i, j] = r.Next(10).ToString();
+            SymmetricMatrix<string> smm = new SymmetricMatrix<string>(3, strarray);
+            smm.ChangeSingleCell(2, 2, "777");
+            Console.WriteLine(smm);
+
+            DateTime[] dtarray = new DateTime[3];
+            for (int i = 0; i < 3; i++)
+                    dtarray[i] = new DateTime((long)(i+1)*1000000000000000000);
+            DiagonalMatrix<DateTime> dm = new DiagonalMatrix<DateTime>(3, dtarray);
+            dm.ChangeSingleCell(2, 2, new DateTime((long)7770000000000000));
+            Console.WriteLine(dm);
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Dya8Task1/SymmetricMatrix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dya8Task1
+{
+    class SymmetricMatrix<T> : SquereMatrix<T>
+    {
+        public SymmetricMatrix()
+        {
+            _matrix = new T[1, 1];
+            _matrix[0, 0] = default(T);
+        }
+
+        public SymmetricMatrix(int n, T[,] arr)
+        {
+            try
+            {
+                if (n == 0)
+                {
+                    throw new Exception("n cann't be 0");
+                }
+                N = n;
+                M = n;
+                _matrix = new T[n, n];
+                for (int i = 0; i < N; i++)
+                    for (int j = 0; j <= i; j++)
+                    {
+                        _matrix[i, j] = arr[i, j];
+                        _matrix[j, i] = arr[i, j];
+                    }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
